1fbc0e43ad09ed0e2b3338f3108f2e0eba91001d
Hemanth | TenaciousX <54387033+HemanthJyothula@users.noreply.github.com>
2024-07-30 12:07:07 +0530

    Created a Basic Rect Frontend and linked with backend ems spring project
    Used react to create a small front end page that shows a table of retrieved contents from backend.
    
    Used axios library of react for the connection between frontend and backend
    
    FrontEnd :
    Created a React App with additonal components and service folder.
    
    Created ListEmployeesComponent.js and EmployeeSerivce.js files
    
    Employee Service uses axios to retrieve details and in ListEmployeeComponent we mount the service and update employee state

---
+++ Employee Management Micro Service Frontend/frontend-ems/src/App.js
@@ -0,0 +1,11 @@
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+
+function App() {
+  return (
+    <div className="container">
+    <ListEmployeeComponent/>
+    </div>
+  );
+}
+
+export default App;
