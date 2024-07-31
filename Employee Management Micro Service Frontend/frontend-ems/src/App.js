9ff96d2cdb7f2a7a3a50031b7299e8a1b2b63221
Hemanth | TenaciousX <54387033+HemanthJyothula@users.noreply.github.com>
2024-07-31 14:02:50 +0530

    Added Header and Footer Components , included React DOM
    Used BrowserRouter, Routes and
    Route from react router DOM
    established a sample mapping for employees which is subject to change

--- Employee Management Micro Service Frontend/frontend-ems/src/App.js
+++ Employee Management Micro Service Frontend/frontend-ems/src/App.js
@@ -1,9 +1,28 @@
 import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
+    <div>
+    
+    <HeaderComponent/>
+    
+    <BrowserRouter>
+
     <div className="container">
-    <ListEmployeeComponent/>
+      <Routes>
+          <Route exact path='/' element={<ListEmployeeComponent/>}></Route>
+          <Route path='/employees' element={<ListEmployeeComponent/>}></Route>
+      </Routes>
+    </div>
+    
+    </BrowserRouter>
+
+    <FooterComponent/>
+
     </div>
   );
 }
